79bbbeef951339064ee3e18360ae91c157510d2b
Douglas Campbell <telldouglas@gmail.com>
2025-03-05 22:06:43 -0500

    Added audio effects to play on button press

--- repeat.js
+++ repeat.js
@@ -1,20 +1,17 @@
 "use strict";
 
-let input = document.getElementById("userInput");
-let button = document.getElementById("submit");
+export let input = document.getElementById("userInput");
+export let button = document.getElementById("submit");
 let result = document.getElementById("result");
 
 button.addEventListener("click", () => {
-  // if (result.innerText = "") {
-  //   result = document.createElement("p");
-  //   // result.style.backgroundColor = "grey";
-  // }
-
   if (input.value == "") {
     result.innerText = "Nothing was entered!";
+    new Audio("./media/radar-ping-306440.mp3").play();
   } else {
     result.innerHTML = "You entered: <br><span class='result-text'></span>";
-    result.querySelector('.result-text').innerText = input.value;
+    result.querySelector(".result-text").innerText = input.value;
+    new Audio("./media/ping-82822.mp3").play();
   }
 
   input.value = "";
